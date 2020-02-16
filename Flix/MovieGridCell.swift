15a0a17b56f4332209efc178f3021246636887b6
ashish <avsavani@gmail.com>
2020-02-15 22:42:25 -0800

    added tab controller and used collection view for poster arrangement

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by ashish on 2/15/20.
+//  Copyright © 2020 AshishSavani. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
